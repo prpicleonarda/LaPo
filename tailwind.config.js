96ab70b273c4aea112701a2484e61b7660bae77e
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-08-27 20:16:40 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Adjust the path if needed
+    "./static/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
